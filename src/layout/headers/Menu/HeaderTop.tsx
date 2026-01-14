a748a9697cda8542055f22fa71a506e45203d3d0
Cizawells <cizawells70@gmail.com>
2026-01-14 15:08:00 +0200

    changes

--- src/layout/headers/Menu/HeaderTop.tsx
+++ src/layout/headers/Menu/HeaderTop.tsx
@@ -1,35 +1,59 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const HeaderTop = ({ style }: any) => {
-   return (
-      <div className={`navbar-top ${style ? "pt-30 rpt-10 navtop--two" : "pt-15 pb-10 bgc-black navtop--one"}`}>
-         <div className="container">
-            <div className={`navtop-inner ${style ? "bgc-gray" : ""}`}>
-               <ul className="topbar-left">
-                  {style ?
-                     <>
-                        <li><b>Call now - <Link href="callto:44-7883055156">44-7883055156</Link></b></li>
-                        <li>123 Clean St, Your City, 10001</li>
-                     </> :
-                     <>
-                        <li><span>Welcome</span>, Need cleaning services? </li>
-                        <li><i className="flaticon-pin"></i>  123 Clean St, Your City, 10001</li>
-                     </>
-                  }
-               </ul>
-               <ul className="topbar-right">
-                  <li className="social-area">
-                     <span>Follow Us - </span>
-                     <Link href="#"><i className="fab fa-facebook-f"></i></Link>
-                     <Link href="#"><i className="fab fa-twitter"></i></Link>
-                     <Link href="#"><i className="fab fa-pinterest-p"></i></Link>
-                     <Link href="#"><i className="fab fa-linkedin-in"></i></Link>
-                  </li>
-               </ul>
-            </div>
-         </div>
+  return (
+    <div
+      className={`navbar-top ${
+        style ? "pt-30 rpt-10 navtop--two" : "pt-15 pb-10 bgc-black navtop--one"
+      }`}
+    >
+      <div className="container">
+        <div className={`navtop-inner ${style ? "bgc-gray" : ""}`}>
+          <ul className="topbar-left">
+            {style ? (
+              <>
+                <li>
+                  <b>
+                    Call now -{" "}
+                    <Link href="callto:44-7883055156">+44 7883 055156</Link>
+                    <Link href="callto:44-7883055156">+44 7883 055156</Link>
+                  </b>
+                </li>
+                <li>26 Milnrow Crescent, Sheffield, S5 9LQ</li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <span>Welcome</span>, Need cleaning services?{" "}
+                </li>
+                <li>
+                  <i className="flaticon-pin"></i> 123 Clean St, Your City,
+                  10001
+                </li>
+              </>
+            )}
+          </ul>
+          <ul className="topbar-right">
+            <li className="social-area">
+              <span>Follow Us - </span>
+              <Link href="#">
+                <i className="fab fa-facebook-f"></i>
+              </Link>
+              <Link href="#">
+                <i className="fab fa-twitter"></i>
+              </Link>
+              <Link href="#">
+                <i className="fab fa-pinterest-p"></i>
+              </Link>
+              <Link href="#">
+                <i className="fab fa-linkedin-in"></i>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-   )
-}
+    </div>
+  );
+};
 
-export default HeaderTop
+export default HeaderTop;
